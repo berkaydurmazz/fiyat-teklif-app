662cf1e039bbdb8054bdf2593c0aa9be0264f382
berkay <berkayd2000@gmail.com>
2023-09-20 01:12:02 +0300

    Users added in frontend

---
+++ webapi/ViewModel/Product/ProductCreateVM.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace webapi.ViewModel.Product
+{
+    public class ProductCreateVM
+    {
+        [Required]
+        public string productName { get; set; }
+        [Required]
+        public string productDescp { get; set; }
+        [Required]
+        public string productSize { get; set; }
+        [Required]
+        public double price { get; set}
+        [Required]
+        public string suppliers { get; set; }
+        [Required]
+        public double kdvRate { get; set; }
+        [Required]
+        public string category { get; set; }
+
+    }
+}
